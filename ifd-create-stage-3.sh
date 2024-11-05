10f5b1afdabd0c7be7d8dbf78184b51af189ea9c
P. Patrick Socha <sochap@gmail.com>
2024-11-05 11:28:12 +0100

    Update: Better logging

--- ifd-create-stage-3.sh
+++ ifd-create-stage-3.sh
@@ -2,8 +2,8 @@
 
 ts() { date -u +%Y%m%d-%H%M%S; }
 
-log() { echo "$(ts) : ${STAGE} : $@" >> progress.log; }
-run() { log $@; $@; }
+log() { INFO="$(ts) : ${STAGE} : $@"; echo "${INFO}"; echo "${INFO}" >> progress.log; }
+run() { log $@; $@; echo "$(ts) : ${STAGE} : $@ : status=$?"; }
 
 STAGE=STAGE-3
 
